2cbd9f37553dbc92535bbbcb6ebc2730c699e51d
Grellheist <grellheist@tutanota.com>
2023-02-10 13:11:00 -0300

    Updated

--- DOM/Section 26/app.js
+++ DOM/Section 26/app.js
@@ -3,6 +3,7 @@ const p2Button = document.querySelector("#playerTwo");
 const resetButton = document.querySelector("#reset");
 const p1Score = document.querySelector("#p1Score");
 const p2Score = document.querySelector("#p2Score");
+const playToSelect = document.querySelector("#playTo");
 
 let p1Points = 0;
 let p2Points = 0;
@@ -30,10 +31,17 @@ p2Button.addEventListener("click", function(){
     p2Score.innerText = p2Points;
 })
 
-resetButton.addEventListener("click", function(){
+playToSelect.addEventListener("change", function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener("click", reset)
+
+    function reset(){
     p1Points = 0;
     p2Points = 0;
     isGameOver = false;
     p1Score.innerText = p1Points;
     p2Score.innerText = p2Points;
-})
+}
