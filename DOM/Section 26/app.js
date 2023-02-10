7b5be44d6a67a9edb2dca475a1196912f3ca829f
Grellheist <grellheist@tutanota.com>
2023-02-10 13:47:39 -0300

    Updated

--- DOM/Section 26/app.js
+++ DOM/Section 26/app.js
@@ -3,6 +3,7 @@ const p1 = {
     button: document.querySelector('#p1Button'),
     display: document.querySelector('#p1Display')
 }
+
 const p2 = {
     score: 0,
     button: document.querySelector('#p2Button'),
