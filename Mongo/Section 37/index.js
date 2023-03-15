bdfd4b524bf76789aa2e0bb28088add3f8a8770a
Grellheist <grellheist@tutanota.com>
2023-03-15 15:09:20 -0300

    Updated

--- Mongo/Section 37/index.js
+++ Mongo/Section 37/index.js
@@ -18,7 +18,6 @@ const movieSchema = new mongoose.Schema({
 const Movie = mongoose.model('Movie', movieSchema);
 // const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
 
-
 // Movie.insertMany([
 //     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
 //     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
