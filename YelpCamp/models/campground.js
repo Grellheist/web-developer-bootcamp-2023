188ac18e9336f93940319ee4a810723b8917d95d
grellheist <grellheist@tutanota.com>
2023-03-27 20:56:37 -0300

    Updated

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const campgroundSchema = new Schema({
+const CampgroundSchema = new Schema({
     title: String,
     image: String,
-    price: String,
+    price: Number,
     description: String,
     location: String
 });
 
-module.exports = mongoose.model("Campground", campgroundSchema);
+module.exports = mongoose.model('Campground', CampgroundSchema);
